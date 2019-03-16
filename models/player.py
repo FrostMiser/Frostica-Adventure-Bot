dfd498b332cb7a78d63146733505ac41cf45d095
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:35:10 -0500

    Add player model

---
+++ models/player.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+
+class Player(Base):
+    __tablename__ = 'player'
+
+    id = Column(Integer, primary_key =True)
+    name = Column(String)
+    health = Column(Integer)
+    mana = Column(Integer)
