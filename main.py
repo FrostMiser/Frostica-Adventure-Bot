292b65355be12e90d1bad8e8a35f745a7b8a8d7d
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:12:58 -0500

    Make the bot functional and cleanup command handling

--- main.py
+++ main.py
@@ -2,43 +2,46 @@ import discord
 from sqlalchemy import create_engine
 import settings
 
-import commands
+
+from commands import cast, char, chop, craft, enter, forage, hunt, inv, map, move, recipes, spellbook, use
 
 
-db_engine = create_engine(settings['database_engine'])
+db_engine = create_engine(settings.settings['database_engine'])
+client = discord.Client()
 
 @client.event
 async def on_message(message):
     message_content = message.content.lower()
-    response = None
+    command =  message_content.split(" ")[0] if message_content.split(" ") else None
+    if not command or message.author == client.user: return
 
-    if message_content.startswith('!map'):
-        response = commands.map.run_command(message, message_content)
-    elif message_content.startswith('!enter'):
+    if command == '!map':
+        response = map.run_command(message, message_content)
+    elif command == '!enter':
         response = commands.enter.run_command(message, message_content)
-    elif message_content.startswith('!move'):
+    elif command == '!move':
         response = commands.move.run_command(message, message_content)
-    elif message_content.startswith('!cast'):
+    elif command == '!cast':
         response = commands.cast.run_command(message, message_content)
-    elif message_content.startswith('!spellbook'):
+    elif command == '!spellbook':
         response = commands.spellbook.run_command(message, message_content)
-    elif message_content.startswith('!forage'):
+    elif command == '!forage':
         response = commands.forage.run_command(message, message_content)
-    elif message_content.startswith('!mine'):
+    elif command == '!mine':
         response = commands.mine.run_command(message, message_content)
-    elif message_content.startswith('!chop'):
+    elif command == '!chop':
         response = commands.chop.run_command(message, message_content)
-    elif message_content.startswith('!hunt'):
+    elif command == '!hunt':
         response = commands.hunt.run_command(message, message_content)
-    elif message_content.startswith('!craft'):
+    elif command == '!craft':
         response = commands.craft.run_command(message, message_content)
-    elif message_content.startswith('!recipes'):
+    elif command == '!recipes':
         response = commands.recipe.run_command(message, message_content)
-    elif message_content.startswith('!use'):
+    elif command == '!use':
         response = commands.use.run_command(message, message_content)
-    elif message_content.startswith('!char'):
+    elif command == '!char':
         response = commands.char.run_command(message, message_content)
-    elif message_content.startswith('!inv'):
+    elif command == '!inv':
         response = commands.inv.run_command(message, message_content)
     else:
         response = 'Unknown command.'
@@ -46,7 +49,7 @@ async def on_message(message):
     if response:
         await client.send_message(message.channel, response)
 
-client.run(settings['api_token'])
+client.run(settings.settings['api_token'])
 
 
 
