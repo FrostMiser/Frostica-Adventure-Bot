34d342fd5bb58169ede00b7336444123079754f3
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:20:53 -0500

    Fixing commands

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import settings
 
 
-from commands import cast, char, chop, craft, enter, forage, hunt, inv, map, move, recipes, spellbook, use
+from commands import cast, char, chop, craft, enter, forage, hunt, inv, map, move, recipes, spellbook, use, setup
 
 
 db_engine = create_engine(settings.settings['database_engine'])
@@ -18,31 +18,31 @@ async def on_message(message):
     if command == '!map':
         response = map.run_command(message, message_content)
     elif command == '!enter':
-        response = commands.enter.run_command(message, message_content)
+        response = enter.run_command(message, message_content)
     elif command == '!move':
-        response = commands.move.run_command(message, message_content)
+        response = move.run_command(message, message_content)
     elif command == '!cast':
-        response = commands.cast.run_command(message, message_content)
+        response = cast.run_command(message, message_content)
     elif command == '!spellbook':
-        response = commands.spellbook.run_command(message, message_content)
+        response = spellbook.run_command(message, message_content)
     elif command == '!forage':
-        response = commands.forage.run_command(message, message_content)
+        response = forage.run_command(message, message_content)
     elif command == '!mine':
-        response = commands.mine.run_command(message, message_content)
+        response = mine.run_command(message, message_content)
     elif command == '!chop':
-        response = commands.chop.run_command(message, message_content)
+        response = chop.run_command(message, message_content)
     elif command == '!hunt':
-        response = commands.hunt.run_command(message, message_content)
+        response = hunt.run_command(message, message_content)
     elif command == '!craft':
-        response = commands.craft.run_command(message, message_content)
+        response = craft.run_command(message, message_content)
     elif command == '!recipes':
-        response = commands.recipe.run_command(message, message_content)
+        response = recipe.run_command(message, message_content)
     elif command == '!use':
-        response = commands.use.run_command(message, message_content)
+        response = use.run_command(message, message_content)
     elif command == '!char':
-        response = commands.char.run_command(message, message_content)
+        response = char.run_command(message, message_content)
     elif command == '!inv':
-        response = commands.inv.run_command(message, message_content)
+        response = inv.run_command(message, message_content)
     else:
         response = 'Unknown command.'
 
