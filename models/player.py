668a6f5e9396da3763647f523547fc01754a0cf4
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:41:25 -0500

    Add player inventory model and cleanup player model

--- models/player.py
+++ models/player.py
@@ -4,7 +4,8 @@ from sqlalchemy import Column, Integer, String
 class Player(Base):
     __tablename__ = 'player'
 
-    id = Column(Integer, primary_key =True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     health = Column(Integer)
     mana = Column(Integer)
+    max_inventory_size = Column(Integer)
---
+++ models/player_inventory.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from models.player import Player
+
+
+class PlayerInventory(Base):
+    __tablename__ = 'player_inventory'
+
+    id = Column(Integer, primary_key=True)
+    player_id = Column (Integer, ForeignKey(Player.id))
+    item_name = Column(String)
+    item_amount = Column(Integer)
+
