4dc50306e45852c5af5d9b3e72a8ad603c9a175b
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 20:53:37 -0500

    Add commands

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import discord
 from sqlalchemy import create_engine
 import settings
 
+import commands
+
+
+
 db_engine = create_engine(settings['database_engine'])
 
 @client.event
@@ -10,20 +14,33 @@ async def on_message(message):
     response = None
 
     if message_content.startswith('!map'):
+        response = commands.map.run_command(message, message_content)
     elif message_content.startswith('!enter'):
-    elif message_content.startswith('!enter'):
+        response = commands.enter.run_command(message, message_content)
     elif message_content.startswith('!move'):
+        response = commands.move.run_command(message, message_content)
     elif message_content.startswith('!cast'):
+        response = commands.cast.run_command(message, message_content)
     elif message_content.startswith('!spellbook'):
+        response = commands.spellbook.run_command(message, message_content)
     elif message_content.startswith('!forage'):
+        response = commands.forage.run_command(message, message_content)
     elif message_content.startswith('!mine'):
+        response = commands.mine.run_command(message, message_content)
     elif message_content.startswith('!chop'):
+        response = commands.chop.run_command(message, message_content)
     elif message_content.startswith('!hunt'):
+        response = commands.hunt.run_command(message, message_content)
     elif message_content.startswith('!craft'):
+        response = commands.craft.run_command(message, message_content)
     elif message_content.startswith('!recipes'):
+        response = commands.recipe.run_command(message, message_content)
     elif message_content.startswith('!use'):
+        response = commands.use.run_command(message, message_content)
     elif message_content.startswith('!char'):
+        response = commands.char.run_command(message, message_content)
     elif message_content.startswith('!inv'):
+        response = commands.inv.run_command(message, message_content)
     else:
         response = 'Unknown command.'
 
