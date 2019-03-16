a97ce70106b8276dfaa06e85fd6dd5312e09fa07
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:45:45 -0500

    Fix spacing

--- models/player_inventory.py
+++ models/player_inventory.py
@@ -6,6 +6,6 @@ class PlayerInventory(Base):
     __tablename__ = 'player_inventory'
 
     id = Column(Integer, primary_key=True)
-    player_id = Column (Integer, ForeignKey(Player.id))
+    player_id = Column(Integer, ForeignKey(Player.id))
     item_name = Column(String)
     item_amount = Column(Integer)
