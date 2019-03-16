ccf5d90444a748cf01d74496a996a791a62172f6
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 20:35:23 -0500

    Change settings, fix variables, and use sqlalchemy

--- default_settings.py
+++ default_settings.py
@@ -1,4 +1,7 @@
 # Rename this file to settings.py and update the values as needed.
 
-database_file_path = '/path/to/database.db'
 
+settings = {
+    'database_engine': 'sqlite:///path/to/database.db',
+    'api_token': 'example'
+}
--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import discord
-import sqlite3
+from sqlalchemy import create_engine
+import settings
 
+db_engine = create_engine(settings['database_engine'])
 
 @client.event
 async def on_message(message):
     message_content = message.content.lower()
     response = None
 
-    if content.startswith('!map'):
-    elif content.startswith('!enter'):
-    elif content.startswith('!enter'):
-    elif content.startswith('!move'):
-    elif content.startswith('!cast'):
-    elif content.startswith('!spellbook'):
-    elif content.startswith('!forage'):
-    elif content.startswith('!mine'):
-    elif content.startswith('!chop'):
-    elif content.startswith('!hunt'):
-    elif content.startswith('!craft'):
-    elif content.startswith('!recipes'):
-    elif content.startswith('!use'):
-    elif content.startswith('!char'):
-    elif content.startswith('!inv'):
+    if message_content.startswith('!map'):
+    elif message_content.startswith('!enter'):
+    elif message_content.startswith('!enter'):
+    elif message_content.startswith('!move'):
+    elif message_content.startswith('!cast'):
+    elif message_content.startswith('!spellbook'):
+    elif message_content.startswith('!forage'):
+    elif message_content.startswith('!mine'):
+    elif message_content.startswith('!chop'):
+    elif message_content.startswith('!hunt'):
+    elif message_content.startswith('!craft'):
+    elif message_content.startswith('!recipes'):
+    elif message_content.startswith('!use'):
+    elif message_content.startswith('!char'):
+    elif message_content.startswith('!inv'):
     else:
         response = 'Unknown command.'
 
     if response:
         await client.send_message(message.channel, response)
 
-client.run(settings.TOKEN)
+client.run(settings['api_token'])
 
 
 
