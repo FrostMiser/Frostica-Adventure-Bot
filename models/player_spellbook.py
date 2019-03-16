cc4a74e1dbdd917881a0d4ae91a3f875f609f226
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-15 21:45:21 -0500

    Add spellbook and spell models

---
+++ models/player_spellbook.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from models.player import Player
+from models.spell import Spell
+
+
+class PlayerSpellbook(Base):
+    __tablename__ = 'player_spellbook'
+
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey(Player.id))
+    spell_id = Column(Integer, ForeignKey(Spell.id))
---
+++ models/spell.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+
+class Player(Base):
+    __tablename__ = 'spell'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    mana_cost = Column(Integer)
