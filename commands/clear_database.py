a3d36f1c8a36716b2b1d7c0ce7065b71589d7ecf
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-16 01:24:33 -0500

    Add ToDo

--- commands/clear_database.py
+++ commands/clear_database.py
@@ -4,7 +4,7 @@ from common.base import Base
 from sqlalchemy.orm import sessionmaker
 from models.player import Player
 
-
+# ToDo Make this not runnable, or have some kind of permission on it
 def run_command(db_engine):
     Base.metadata.drop_all(bind=db_engine)
     response = 'Database cleared.'
