49487cd32b153344c87172a767034f065afcf25a
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-17 21:24:32 -0500

    Fix foreign key

--- models/recipe_ingredient.py
+++ models/recipe_ingredient.py
@@ -7,5 +7,5 @@ class RecipeIngredient(Base):
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
-    recipe = Column(Integer, ForeignKey(Player.id))
+    recipe_id = Column(Integer, ForeignKey(Recipe.id))
     item = relationship('models.item.Item')
