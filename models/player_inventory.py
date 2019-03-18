7996ec6a9642bb2cd078d409277b35d9c656ae77
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-17 22:41:52 -0500

    Add ToDo

--- models/player_inventory.py
+++ models/player_inventory.py
@@ -5,7 +5,7 @@ from models.item import Item
 from sqlalchemy import Column, Integer, String, UniqueConstraint, ForeignKey
 from sqlalchemy.orm import relationship
 
-
+# ToDo Consider renaming this, should this be something like PlayerInventoryItem?
 class PlayerInventory(Base):
     __tablename__ = 'player_inventory'
 
