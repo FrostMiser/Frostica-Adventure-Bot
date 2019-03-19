3c7fdae248c4983a2eaa149691b662ef906b51aa
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-18 22:52:29 -0500

    Fix issue with player id

--- common/initialize.py
+++ common/initialize.py
@@ -8,6 +8,6 @@ def initialize_player(name, player_id):
     session = session_maker()
     player = session.query(Player).filter(Player.id == player_id).first()
     if not player:
-        new_player = Player(id=id, name=name)
+        new_player = Player(id=player_id, name=name)
         session.add(new_player)
         session.commit()
