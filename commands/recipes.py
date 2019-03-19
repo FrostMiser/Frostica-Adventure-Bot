b887d2e2df510b51435be70552da78c4ba7979e2
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-18 19:22:10 -0500

    Add !recipes and remove unused helpers

--- commands/recipes.py
+++ commands/recipes.py
@@ -1,5 +1,25 @@
+from common.database import db_engine
+from common.helpers import get_session
+from models.recipe import Recipe
+from models.recipe_ingredient import RecipeIngredient
 
 
 def run_command(message, message_content):
-    response = 'This oommand is not yet supported.'
+    session = get_session()
+    recipe_name = ' '.join(message_content.split(" ")[1:]) if len(message_content.split(" ")) > 1 else None
+    if recipe_name:
+        recipe = session.query(Recipe).filter(Recipe.name == recipe_name).first()
+        if recipe:
+            recipe_ingredients = session.query(RecipeIngredient).filter(RecipeIngredient.recipe_id == recipe.id).all()
+            response = '__Recipe For {}__'.format(recipe.name)
+            for recipe_ingredient in recipe_ingredients:
+                response += '\n{}({})'.format(recipe_ingredient.item.name, recipe_ingredient.item_amount)
+        else:
+            response = 'No recipe by that name was found.'
+    else:
+        recipes = session.query(Recipe).all()
+        response = '__Recipes__'
+        for recipe in recipes:
+            response += '\n{}'.format(recipe.name)
+
     return response
--- common/helpers.py
+++ common/helpers.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from models.player import Player
 from models.item import Item
 from models.player_inventory import PlayerInventory
+from models.recipe_ingredient import RecipeIngredient
 from common.database import db_engine
 '''
 This file contains helpers for making it easier to get data and do other things. 
@@ -20,28 +21,11 @@ def get_session():
     session = session_maker()
     return session
 
+
 def get_item(name):
     session_maker = sessionmaker(bind=db_engine)
     session = session_maker()
     item = session.query(Item).filter(Item.name == name).first()
-    session.expunge(item)
-    session.close()
-    return item
-
-
-def get_recipe(name):
-    session_maker = sessionmaker(bind=db_engine)
-    session = session_maker()
-    item = session.query(Recipe).filter(Item.name == name).first()
-    session.expunge(item)
-    session.close()
-    return item
-
-
-def get_recipe_ingredients_by_recipe_id(recipe_id):
-    session_maker = sessionmaker(bind=db_engine)
-    session = session_maker()
-    item = session.query(RecipeIngredient).filter(RecipieIngredient.recipe_id == recipe_id).all()
     session.expunge(item)
     session.close()
     return item
