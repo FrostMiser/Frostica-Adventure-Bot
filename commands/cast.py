a5c51042939e8c8c551d822dd40368e72715dfd5
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-20 23:31:03 -0500

    Add teleport spell

--- commands/cast.py
+++ commands/cast.py
@@ -1,3 +1,24 @@
+from models.player import Player
+from common.helpers import get_session
+
+# ToDo Add mana cost and get spells from database. This version of teleport is temporary to help get around the map
 def run_command(message, message_content):
-    response = 'This command is not yet supported.'
+    spell_name = ' '.join(message_content.split(" ")[1:2]) if len(message_content.split(" ")) > 1 else None
+
+    if not spell_name:
+        return 'You must say what spell you want to case with !cast <spell>'
+    if spell_name == 'teleport':
+        location_x = ' '.join(message_content.split(" ")[2:3]) if len(message_content.split(" ")) > 2 else None
+        location_y = ' '.join(message_content.split(" ")[3:4]) if len(message_content.split(" ")) > 3 else None
+        if location_x and location_y and location_x.isnumeric() and location_y.isnumeric() \
+                    and 0 < int(location_x) < 1000 and 0 < int(location_y) < 1000:
+            session = get_session()
+            player = session.query(Player).filter(Player.id == message.author.id).first()
+            player.x = location_x
+            player.y = location_y
+            session.commit()
+            response = 'You have teleported to {} {}.'.format(location_x, location_y)
+        else:
+            response = 'You must say where you want to teleport to with !cast teleport x y.'
+
     return response
