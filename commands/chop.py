c06515b00e347c2774be05a281509b167d34722b
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-22 23:02:26 -0500

    Make activities use hunger and thirst

--- commands/chop.py
+++ commands/chop.py
@@ -4,7 +4,7 @@ from models.item import Item
 from models.player import Player
 from models.player_inventory import PlayerInventory
 from common.world import get_tile_from
-
+from common.helpers import drain_player_hunger_and_thirst
 
 def run_command(message, session):
     player = session.query(Player).filter(Player.id == message.author.id).first()
@@ -20,6 +20,7 @@ def run_command(message, session):
         chopped_item = session.query(Item).filter(Item.id == chopped_item_id).first()
 
         player_inventory = session.query(PlayerInventory).filter(PlayerInventory.item_id == chopped_item.id).first()
+        drain_player_hunger_and_thirst(player)
 
         if player_inventory:
             player_inventory.item_amount += 1
