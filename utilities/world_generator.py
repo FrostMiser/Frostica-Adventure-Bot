28e82ac87b0dd7a48cd3d549103e6bdc218126da
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-22 20:37:38 -0500

    Add world generator utility

---
+++ utilities/world_generator.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import json
+
+
+# Reads an image and translates it to a world map in world.json
+def generate_world():
+    map_image = Image.open('world.png', 'r') # Must be a square image
+    world_width = 1000  # Set this to the size of the map being generated, should match the pixel width of the image
+    pixels = list(map_image.getdata())
+    row = []
+    world = []
+    for index, pixel in enumerate(pixels):
+        pixel_total = pixel[0]+pixel[1]+pixel[2]
+        if pixel_total <= 50:
+            output = 2
+        elif 51 <= pixel_total <= 400:
+            if 300 < pixel_total < 350:
+                output = 3
+            else:
+                output = 1
+        elif 400 <= pixel_total < 550:
+            output = 4
+        else:
+            output = 0
+        row.append(output)
+        if len(row) % world_width == 0:
+            world.append(row)
+            row = []
+
+    output = open('../world.json', 'w')
+    json.dumps(world)
+    output.write(world.__str__())
+    print('Finished generating world.json file.')
+
+
+generate_world()
