abede907c571f59066a72dfb693344e57bac3550
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-22 20:31:27 -0500

    Code cleanup

--- commands/cast.py
+++ commands/cast.py
@@ -7,12 +7,12 @@ def run_command(message, message_content):
     spell_name = ' '.join(message_content.split(" ")[1:2]) if len(message_content.split(" ")) > 1 else None
 
     if not spell_name:
-        return 'You must say what spell you want to case with !cast <spell>'
+        return 'You must say what spell you want to cast with !cast <spell>'
     if spell_name == 'teleport':
         location_x = ' '.join(message_content.split(" ")[2:3]) if len(message_content.split(" ")) > 2 else None
         location_y = ' '.join(message_content.split(" ")[3:4]) if len(message_content.split(" ")) > 3 else None
         if location_x and location_y and location_x.isnumeric() and location_y.isnumeric() \
-                    and 0 < int(location_x) < 1000 and 0 < int(location_y) < 1000:
+                and 0 <= int(location_x) < 10000 and 0 <= int(location_y) < 10000:
             session = get_session()
             player = session.query(Player).filter(Player.id == message.author.id).first()
             player.x = location_x
