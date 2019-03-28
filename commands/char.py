adf05ccf32820b45b6d48128fc91daf4b3ce3a28
FrostMiser <FrostMiser@users.noreply.github.com>
2019-03-27 19:19:00 -0500

    Use images and rounded numbers

--- commands/char.py
+++ commands/char.py
@@ -4,11 +4,11 @@ from models.player import Player
 def run_command(message, session):
     player = session.query(Player).filter(Player.id == message.author.id).first()
     response = '__{}__'.format(player.name)
-    response += '\nHealth {}%'.format((player.health/player.max_health)*100)
+    response += '\n:heart: Health: {}%'.format(round((player.health/player.max_health)*100))
     if player.max_mana > 0:
-        response += '\nMana {}%'.format((player.mana/player.max_mana)*100)
-    response += '\nHunger {}%'.format((player.hunger/player.max_hunger)*100)
-    response += '\nThirst {}%'.format((player.thirst/player.max_thirst)*100)
+        response += '\n:sparkles: Mana: {}%'.format(round((player.mana/player.max_mana)*100))
+    response += '\n:cake: Hunger: {}%'.format(round((player.hunger/player.max_hunger)*100))
+    response += '\n:droplet: Thirst: {}%'.format(round((player.thirst/player.max_thirst)*100))
 
     if player.main_hand_item:
         response += '\n\n__Equipment__'
