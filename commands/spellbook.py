9496bb531e23cc8ae7f9f6c72a12d0365dd8b6bc
FrostMiser <FrostMiser@users.noreply.github.com>
2020-05-24 20:28:48 -0500

    Add output for spell book

--- commands/spellbook.py
+++ commands/spellbook.py
@@ -1,5 +1,14 @@
 
 
 def run_command():
-    response = 'This command is not yet supported.'
+    response = '__Book of Spells__'
+    spells = {
+        'convert': 'Convert magic ore into mana.',
+        'teleport': 'Teleport up to 10 tiles away.',
+    }
+
+    response = '__Spell Book__'
+    for command, description in spells.items():
+        response += '\n**{}**: `{}`'.format(command, description)
+
     return response
